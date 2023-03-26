40f69422329b9130337d394d604a33905af4c674
Natali <nbyka@mail.ru>
2023-03-26 21:58:26 +0300

    EvenNumberToN

---
+++ Task8_EvenNumberToN/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+if(i % 2 ==0)
+{
+    Console.Write(i);
+    Console.Write(" ");
+}
