6b82caa58780983fc07246b88239d533c1fa5bf1
Natali <nbyka@mail.ru>
2023-03-26 20:30:55 +0300

    Min and Max

---
+++ Task2_MaxAndMin/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int FirstNumber = int.Parse(Console.ReadLine());
+int SecondNumber = int.Parse(Console.ReadLine());
+
+int max = FirstNumber;
+int min = FirstNumber;
+
+if(SecondNumber > max)
+{
+    Console.Write("max = ");
+    Console.WriteLine(max = SecondNumber);
+    Console.Write("min = ");
+    Console.WriteLine(min = FirstNumber);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(max = FirstNumber);
+    Console.Write("min = ");
+    Console.WriteLine(min = SecondNumber);
+}
